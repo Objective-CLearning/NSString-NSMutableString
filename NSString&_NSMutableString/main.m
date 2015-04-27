317737ad4d9a9414d7f42554235ee3eb7cf43a9a
曾偉亮 <Zengwei-liang@cengweiliangdeMacBook-Air.local>
2015-04-27 20:01:37 +0800

    Initial Commit

---
+++ NSString&_NSMutableString/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  NSString&_NSMutableString
+//
+//  Created by 曾偉亮 on 2015/4/27.
+//  Copyright (c) 2015年 曾偉亮. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
